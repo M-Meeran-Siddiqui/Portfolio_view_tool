e536d7910c65525abdeb4f1193c1ef68fc3f3b79
Meeran <mee.sidd.1919@gmail.com>
2025-08-11 09:27:08 +0530

    Portfolio tool initial commit

---
+++ lib/portfolio_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "portfolio_view_tool/version"
+require_relative "portfolio_view_tool/renderer"
+
+module PortfolioViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
